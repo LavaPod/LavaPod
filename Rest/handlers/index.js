bc9dd7e9cc03be86cbec0f6adf5621306168412e
Matthieu <matthieu974fr.pignolet@gmail.com>
2020-01-20 17:16:20 +0400

    some changes :)

--- Rest/handlers/index.js
+++
@@ -1,8 +0,0 @@
-
-/** @typedef {Object} Handlers
- * @prop {import("./httpErrorsHandler")} errorsHandler Our custom http errors handler
- * The availavle http handlers.
- */
-module.exports = {
-  errorsHandler: require('./httpErrorsHandler')
-}
